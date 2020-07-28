bf3ebf231b7205386dab1efe390a1a2c04a142b1
warren.si <sizhongming@163.com>
2020-07-28 16:15:43 +0800

    完善登录样式

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -22,52 +22,71 @@ class Login extends React.Component<MainLayoutProps> {
   render() {
     const { main, history } = this.props;
     return (
-      <div className="login-container">
-        <div>
-          <img alt="" src="img/header_gh.png" width="132px" height="135px"></img>
-          <div>无锡市人民检察院知识产权检察监督信息平台</div>
-        </div>
-        <div>
+      <>
+        <video muted loop autoPlay className="bg-video">
+          <source src="bg.mp4" type="video/mp4" />
+        </video>
+        <div className="login-container">
+          <div>
+            <img
+              alt=""
+              src="img/login_logo.png"
+              width="1051px"
+              height="253px"
+            ></img>
+          </div>
           <div>
             <div>
-              <Input
-                placeholder="请输入用户名"
-                style={{ width: "398px", height: "42px" }}
-                prefix={<UserOutlined translate="true" />}
-                onChange={e =>
-                  this.setState({
-                    username: e.currentTarget.value
-                  })
+              <div>
+                <Input
+                  placeholder="请输入用户名"
+                  style={{ width: "398px", height: "42px" }}
+                  prefix={<UserOutlined translate="true" />}
+                  onChange={e =>
+                    this.setState({
+                      username: e.currentTarget.value
+                    })
+                  }
+                />
+              </div>
+              <div>
+                <Input
+                  type="password"
+                  onPressEnter={() =>
+                    main.doLogin(
+                      this.state.username,
+                      this.state.password,
+                      history
+                    )
+                  }
+                  style={{ width: "398px", height: "42px" }}
+                  placeholder="请输入密码"
+                  prefix={<UserOutlined translate="true" />}
+                  onChange={e =>
+                    this.setState({
+                      password: e.currentTarget.value
+                    })
+                  }
+                />
+              </div>
+              <ColorButton
+                width="398px"
+                onClick={() =>
+                  main.doLogin(
+                    this.state.username,
+                    this.state.password,
+                    history
+                  )
                 }
-              />
+                disabled={main.logining}
+                bgColor="#2E74EE"
+              >
+                登录
+              </ColorButton>
             </div>
-            <div>
-              <Input
-                type="password"
-                onPressEnter={() => main.doLogin(this.state.username, this.state.password, history)}
-                style={{ width: "398px", height: "42px" }}
-                placeholder="请输入密码"
-                prefix={<UserOutlined translate="true" />}
-                onChange={e =>
-                  this.setState({
-                    password: e.currentTarget.value
-                  })
-                }
-              />
-            </div>
-            <ColorButton
-              width="398px"
-              onClick={() =>
-                main.doLogin(this.state.username, this.state.password, history)
-              }
-              disabled={main.logining}
-              bgColor="#2E74EE"
-            >
-              登录
-            </ColorButton>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
