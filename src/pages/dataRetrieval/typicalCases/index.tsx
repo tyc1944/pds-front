7f7898d7c9c70478e38bfdafe39a9e6bfd34e2ca
warren.si <sizhongming@163.com>
2020-07-31 16:29:16 +0800

    update text

--- src/pages/dataRetrieval/typicalCases/index.tsx
+++ src/pages/dataRetrieval/typicalCases/index.tsx
@@ -10,46 +10,64 @@ import { RouteComponentProps } from "react-router-dom";
 
 const { TabPane } = Tabs;
 
-class LawsAndRegulationsDataRetrieval extends React.Component<RouteComponentProps> {
+class LawsAndRegulationsDataRetrieval extends React.Component<
+  RouteComponentProps
+> {
+  state = {
+    activeTabIndex: "1"
+  };
 
-    state = {
-        activeTabIndex: "1"
-    }
+  onTabChange = (key: string) => {
+    this.setState({
+      activeTabIndex: key
+    });
+  };
 
-    onTabChange = (key: string) => {
-        this.setState({
-            activeTabIndex: key
-        })
-    }
+  onDetailClick = (id: number) => {
+    this.props.history.push(`/index/data/retrieval/typical/${id}`);
+  };
 
-    onDetailClick = (id: number) => {
-        this.props.history.push(`/index/data/retrieval/typical/${id}`)
-    }
-
-    render() {
-        return <div style={{
-            display: "flex",
-            height: "100%",
-            flexDirection: 'column'
-        }}>
-            <Breadscrum data={["资料检索", "典型案例"]}></Breadscrum>
-            <BoxContainer>
-                <BoxContainerInner flex={1} noPadding noBorder>
-                    <Tabs defaultActiveKey="1" onChange={this.onTabChange}>
-                        <TabPane tab={<TableNameWithNumber name="法院案例" count={0} />} key="1">
-                            <CourtCase onDetailClick={this.onDetailClick}></CourtCase>
-                        </TabPane>
-                        <TabPane tab={<TableNameWithNumber name="检查案例" count={0} />} key="2">
-                            <ProcuratorateCase onDetailClick={this.onDetailClick}></ProcuratorateCase>
-                        </TabPane>
-                        <TabPane tab={<TableNameWithNumber name="行政执法案例" count={0} />} key="3">
-                            <AdministratorCase onDetailClick={this.onDetailClick}></AdministratorCase>
-                        </TabPane>
-                    </Tabs>
-                </BoxContainerInner>
-            </BoxContainer>
-        </div>
-    }
+  render() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          height: "100%",
+          flexDirection: "column"
+        }}
+      >
+        <Breadscrum data={["资料检索", "典型案例"]}></Breadscrum>
+        <BoxContainer>
+          <BoxContainerInner flex={1} noPadding noBorder>
+            <Tabs defaultActiveKey="1" onChange={this.onTabChange}>
+              <TabPane
+                tab={<TableNameWithNumber name="法院案例" count={0} />}
+                key="1"
+              >
+                <CourtCase onDetailClick={this.onDetailClick}></CourtCase>
+              </TabPane>
+              <TabPane
+                tab={<TableNameWithNumber name="案例" count={0} />}
+                key="2"
+              >
+                <ProcuratorateCase
+                  onDetailClick={this.onDetailClick}
+                ></ProcuratorateCase>
+              </TabPane>
+              <TabPane
+                tab={<TableNameWithNumber name="行政执法案例" count={0} />}
+                key="3"
+              >
+                <AdministratorCase
+                  onDetailClick={this.onDetailClick}
+                ></AdministratorCase>
+              </TabPane>
+            </Tabs>
+          </BoxContainerInner>
+        </BoxContainer>
+      </div>
+    );
+  }
 }
 
 export default LawsAndRegulationsDataRetrieval;
--- src/pages/dataRetrieval/wuxiCases/index.tsx
+++ src/pages/dataRetrieval/wuxiCases/index.tsx
@@ -11,49 +11,74 @@ import { RouteComponentProps } from "react-router-dom";
 
 const { TabPane } = Tabs;
 
-class LawsAndRegulationsDataRetrieval extends React.Component<RouteComponentProps> {
+class LawsAndRegulationsDataRetrieval extends React.Component<
+  RouteComponentProps
+> {
+  state = {
+    activeTabIndex: "1"
+  };
 
-    state = {
-        activeTabIndex: "1"
-    }
+  onTabChange = (key: string) => {
+    this.setState({
+      activeTabIndex: key
+    });
+  };
 
-    onTabChange = (key: string) => {
-        this.setState({
-            activeTabIndex: key
-        })
-    }
+  onDetailClick = (id: number, category: string) => {
+    this.props.history.push(`/index/data/retrieval/wuxi/${category}/${id}`);
+  };
 
-    onDetailClick = (id: number, category: string) => {
-        this.props.history.push(`/index/data/retrieval/wuxi/${category}/${id}`)
-    }
-
-    render() {
-        return <div style={{
-            display: "flex",
-            height: "100%",
-            flexDirection: 'column'
-        }}>
-            <Breadscrum data={["资料检索", "无锡案例"]}></Breadscrum>
-            <BoxContainer>
-                <BoxContainerInner flex={1} noPadding noBorder>
-                    <Tabs defaultActiveKey="1" onChange={this.onTabChange}>
-                        <TabPane tab={<TableNameWithNumber name="检查文书" count={0} />} key="1">
-                            <ProcuratorialDocument onDetailClick={this.onDetailClick}></ProcuratorialDocument>
-                        </TabPane>
-                        <TabPane tab={<TableNameWithNumber name="法院文书" count={0} />} key="2">
-                            <CourtDocument onDetailClick={this.onDetailClick}></CourtDocument>
-                        </TabPane>
-                        <TabPane tab={<TableNameWithNumber name="行政案件" count={0} />} key="3">
-                            <AdministrativeCase onDetailClick={this.onDetailClick}></AdministrativeCase>
-                        </TabPane>
-                        <TabPane tab={<TableNameWithNumber name="执行案件" count={0} />} key="4">
-                            <EnforcementCase onDetailClick={this.onDetailClick}></EnforcementCase>
-                        </TabPane>
-                    </Tabs>
-                </BoxContainerInner>
-            </BoxContainer>
-        </div >
-    }
+  render() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          height: "100%",
+          flexDirection: "column"
+        }}
+      >
+        <Breadscrum data={["资料检索", "无锡案例"]}></Breadscrum>
+        <BoxContainer>
+          <BoxContainerInner flex={1} noPadding noBorder>
+            <Tabs defaultActiveKey="1" onChange={this.onTabChange}>
+              <TabPane
+                tab={<TableNameWithNumber name="文书" count={0} />}
+                key="1"
+              >
+                <ProcuratorialDocument
+                  onDetailClick={this.onDetailClick}
+                ></ProcuratorialDocument>
+              </TabPane>
+              <TabPane
+                tab={<TableNameWithNumber name="法院文书" count={0} />}
+                key="2"
+              >
+                <CourtDocument
+                  onDetailClick={this.onDetailClick}
+                ></CourtDocument>
+              </TabPane>
+              <TabPane
+                tab={<TableNameWithNumber name="行政案件" count={0} />}
+                key="3"
+              >
+                <AdministrativeCase
+                  onDetailClick={this.onDetailClick}
+                ></AdministrativeCase>
+              </TabPane>
+              <TabPane
+                tab={<TableNameWithNumber name="执行案件" count={0} />}
+                key="4"
+              >
+                <EnforcementCase
+                  onDetailClick={this.onDetailClick}
+                ></EnforcementCase>
+              </TabPane>
+            </Tabs>
+          </BoxContainerInner>
+        </BoxContainer>
+      </div>
+    );
+  }
 }
 
 export default LawsAndRegulationsDataRetrieval;
