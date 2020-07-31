b0c1fcef2ad3985db0579b4214f8aef784bce9f9
warren.si <sizhongming@163.com>
2020-07-31 11:24:50 +0800

    完善导出罗i及

--- src/pages/dataAnalysis/citywide/index.tsx
+++ src/pages/dataAnalysis/citywide/index.tsx
@@ -3,30 +3,44 @@ import Breadscrum from "components/breadscrum";
 import { BoxContainer, BoxContainerInner } from "components/layout";
 import { TableListOpsHelper } from "components/table/tableListOpsContext";
 import { Row, Col } from "antd";
-import { OptionsDateRangePicker, TableListOpsValueType } from "components/table/tableListOpsComponents";
+import {
+  OptionsDateRangePicker,
+  TableListOpsValueType
+} from "components/table/tableListOpsComponents";
 import { ChartRow } from "../components";
 import {
-    CaseRankChart, CaseAreaChart, CaseCategoryChart, CaseReasonChart, ProcuratorateJobChart, ProcuratorJobChart, TrialProcedureChart, TrialDurationChart
+  CaseRankChart,
+  CaseAreaChart,
+  CaseCategoryChart,
+  CaseReasonChart,
+  ProcuratorateJobChart,
+  ProcuratorJobChart,
+  TrialProcedureChart,
+  TrialDurationChart
 } from "./charts";
 
-import { SubstantiveLawChart, ProceduralLawChart } from "../nationwide/charts"
+import { SubstantiveLawChart, ProceduralLawChart } from "../nationwide/charts";
+import { ColorButton } from "components/buttons";
+import { axios } from "utils/RequestUtil";
 
 class CitywideDataAnalysis extends React.Component {
+  state = {
+    changed: [] as TableListOpsValueType[]
+  };
 
-    state = {
-        changed: [] as TableListOpsValueType[],
-    }
-
-    render() {
-        return <div style={{
-            display: "flex",
-            height: "100%",
-            flexDirection: 'column'
-        }}>
-            <Breadscrum data={["决策辅助"]}></Breadscrum>
-            <BoxContainer>
-                {/* <BoxContainerInner>
-                    <TableListOpsHelper
+  render() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          height: "100%",
+          flexDirection: "column"
+        }}
+      >
+        <Breadscrum data={["决策辅助"]}></Breadscrum>
+        <BoxContainer>
+          <BoxContainerInner>
+            {/* <TableListOpsHelper
                         onChanged={changed =>
                             this.setState({
                                 changed
@@ -50,18 +64,58 @@ class CitywideDataAnalysis extends React.Component {
                                 </Row>
                             </div>
                         </div>
-                    </TableListOpsHelper>
-                </BoxContainerInner> */}
-                <BoxContainerInner flex={1} noPadding noBorder>
-                    <ChartRow leftContent={<CaseRankChart></CaseRankChart>} rightContent={<CaseAreaChart></CaseAreaChart>}></ChartRow>
-                    <ChartRow leftContent={<CaseCategoryChart></CaseCategoryChart>} rightContent={<CaseReasonChart></CaseReasonChart>}></ChartRow>
-                    <ChartRow leftContent={<ProcuratorateJobChart></ProcuratorateJobChart>} rightContent={<ProcuratorJobChart></ProcuratorJobChart>}></ChartRow>
-                    <ChartRow leftContent={<TrialProcedureChart></TrialProcedureChart>} rightContent={<TrialDurationChart></TrialDurationChart>}></ChartRow>
-                    <ChartRow leftContent={<SubstantiveLawChart></SubstantiveLawChart>} rightContent={<ProceduralLawChart></ProceduralLawChart>}></ChartRow>
-                </BoxContainerInner>
-            </BoxContainer>
-        </div>
-    }
+                    </TableListOpsHelper> */}
+            <div
+              style={{
+                margin: "18px 0px 0 0",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                height: "100%"
+              }}
+            >
+              <ColorButton
+                onClick={() => {
+                  axios
+                    .get(`/api/statistics/city/export`, {
+                      responseType: "blob"
+                    })
+                    .then(res => {
+                      var fileDownload = require("js-file-download");
+                      fileDownload(res.data, "无锡市知识产权案件分析报告.docx");
+                    });
+                }}
+              >
+                导出
+              </ColorButton>
+            </div>
+          </BoxContainerInner>
+          <BoxContainerInner flex={1} noPadding noBorder>
+            <ChartRow
+              leftContent={<CaseRankChart></CaseRankChart>}
+              rightContent={<CaseAreaChart></CaseAreaChart>}
+            ></ChartRow>
+            <ChartRow
+              leftContent={<CaseCategoryChart></CaseCategoryChart>}
+              rightContent={<CaseReasonChart></CaseReasonChart>}
+            ></ChartRow>
+            <ChartRow
+              leftContent={<ProcuratorateJobChart></ProcuratorateJobChart>}
+              rightContent={<ProcuratorJobChart></ProcuratorJobChart>}
+            ></ChartRow>
+            <ChartRow
+              leftContent={<TrialProcedureChart></TrialProcedureChart>}
+              rightContent={<TrialDurationChart></TrialDurationChart>}
+            ></ChartRow>
+            <ChartRow
+              leftContent={<SubstantiveLawChart></SubstantiveLawChart>}
+              rightContent={<ProceduralLawChart></ProceduralLawChart>}
+            ></ChartRow>
+          </BoxContainerInner>
+        </BoxContainer>
+      </div>
+    );
+  }
 }
 
 export default CitywideDataAnalysis;
