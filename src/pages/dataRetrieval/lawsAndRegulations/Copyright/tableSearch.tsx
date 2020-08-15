57c363281c3f8f61e059d0c55e0bbde55eed86f0
warren.si <sizhongming@163.com>
2020-08-15 10:20:49 +0800

    fix issue

--- src/pages/dataRetrieval/lawsAndRegulations/Copyright/tableSearch.tsx
+++ src/pages/dataRetrieval/lawsAndRegulations/Copyright/tableSearch.tsx
@@ -1,71 +1,109 @@
 import React from "react";
 import { TableListOpsHelper } from "components/table/tableListOpsContext";
 import {
-    TableListOpsValueType,
-    InputWithoutIcon,
-    SingleSelectionGroup,
-    OptionsDateRangePicker
+  TableListOpsValueType,
+  InputWithoutIcon,
+  SingleSelectionGroup
 } from "components/table/tableListOpsComponents";
 import { Row, Col } from "antd";
 import { ColorButton } from "components/buttons";
 
 export const TableSearch = (props: {
-    onSearch: (changed: TableListOpsValueType[]) => void;
+  onSearch: (changed: TableListOpsValueType[]) => void;
 }) => {
+  const [changed, setChanged] = React.useState([] as TableListOpsValueType[]);
+  const [key, setKey] = React.useState(Date.now());
 
-    const [changed, setChanged] = React.useState([] as TableListOpsValueType[])
-    const [key, setKey] = React.useState(Date.now());
-
-    return <TableListOpsHelper
-        key={key}
-        onChanged={changed => {
-            setChanged(changed as [])
-        }}
-        initData={changed}
+  return (
+    <TableListOpsHelper
+      key={key}
+      onChanged={changed => {
+        setChanged(changed as []);
+      }}
+      initData={changed}
     >
-        <div className="table-search-container" style={{
-            margin: '18px 0px',
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            height: '100%'
-        }}>
-            <div>
-                <Row>
-                    <Col span={12}>
-                        <div style={{
-                            display: "flex",
-                        }}>
-                            <InputWithoutIcon style={{ width: "290px" }} name="keyword"
-                                placeholder="输入关键词进行搜索"></InputWithoutIcon>
-                            <ColorButton width="76px" bgColor="#4084F0"
-                                onClick={() => props.onSearch(changed)}>查询</ColorButton>
-                            <ColorButton width="76px" bgColor="#FFFFFF" fontColor="#72757B" onClick={() => {
-                                setChanged([])
-                                setKey(Date.now())
-                            }}>清空</ColorButton>
-                        </div>
-                    </Col>
-                </Row>
-            </div>
-            <div>
-                <Row>
-                    <Col xl={2} xs={4} style={{ color: '#9099A2' }}>修订年份</Col>
-                    <Col>
-                        <SingleSelectionGroup name="revisionYear" defaultValue="不限"
-                            selectItems={["不限", "2013年", "2010年", "2002年", "2001年", "1999年", "1991年"]}></SingleSelectionGroup>
-                    </Col>
-                </Row>
-            </div>
-            <div>
-                <Row>
-                    <Col xl={2} xs={4} style={{ color: '#9099A2' }}>类别</Col>
-                    <Col xl={22} xs={20}>
-                        <SingleSelectionGroup name="subcategory" defaultValue="不限"
-                            selectItems={["不限", "法律", "行政法规"]}></SingleSelectionGroup>
-                    </Col>
-                </Row>
-            </div>
+      <div
+        className="table-search-container"
+        style={{
+          margin: "18px 0px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          height: "100%"
+        }}
+      >
+        <div>
+          <Row>
+            <Col span={12}>
+              <div
+                style={{
+                  display: "flex"
+                }}
+              >
+                <InputWithoutIcon
+                  style={{ width: "290px" }}
+                  name="keyword"
+                  placeholder="输入关键词进行搜索"
+                ></InputWithoutIcon>
+                <ColorButton
+                  width="76px"
+                  bgColor="#4084F0"
+                  onClick={() => props.onSearch(changed)}
+                >
+                  查询
+                </ColorButton>
+                <ColorButton
+                  width="76px"
+                  bgColor="#FFFFFF"
+                  fontColor="#72757B"
+                  onClick={() => {
+                    setChanged([]);
+                    setKey(Date.now());
+                  }}
+                >
+                  清空
+                </ColorButton>
+              </div>
+            </Col>
+          </Row>
         </div>
+        <div>
+          <Row>
+            <Col xl={2} xs={4} style={{ color: "#9099A2" }}>
+              修订年份
+            </Col>
+            <Col>
+              <SingleSelectionGroup
+                name="revisionYear"
+                defaultValue="不限"
+                selectItems={[
+                  "不限",
+                  "2013年",
+                  "2010年",
+                  "2002年",
+                  "2001年",
+                  "1999年",
+                  "1991年"
+                ]}
+              ></SingleSelectionGroup>
+            </Col>
+          </Row>
+        </div>
+        <div>
+          <Row>
+            <Col xl={2} xs={4} style={{ color: "#9099A2" }}>
+              类别
+            </Col>
+            <Col xl={22} xs={20}>
+              <SingleSelectionGroup
+                name="subcategory"
+                defaultValue="不限"
+                selectItems={["不限", "法律", "行政法规"]}
+              ></SingleSelectionGroup>
+            </Col>
+          </Row>
+        </div>
+      </div>
     </TableListOpsHelper>
-}
+  );
+};
--- src/pages/dataRetrieval/wuxiCases/administrativeCase/tableSearch.tsx
+++ src/pages/dataRetrieval/wuxiCases/administrativeCase/tableSearch.tsx
@@ -1,70 +1,98 @@
 import React from "react";
 import { TableListOpsHelper } from "components/table/tableListOpsContext";
 import {
-    TableListOpsValueType,
-    InputWithoutIcon,
-    SingleSelectionGroup,
-    OptionsDateRangePicker
+  TableListOpsValueType,
+  InputWithoutIcon,
+  OptionsDateRangePicker
 } from "components/table/tableListOpsComponents";
 import { Row, Col } from "antd";
 import { ColorButton } from "components/buttons";
 
 export const TableSearch = (props: {
-    onSearch: (changed: TableListOpsValueType[]) => void;
+  onSearch: (changed: TableListOpsValueType[]) => void;
 }) => {
+  const [changed, setChanged] = React.useState([] as TableListOpsValueType[]);
+  const [key, setKey] = React.useState(Date.now());
 
-    const [changed, setChanged] = React.useState([] as TableListOpsValueType[])
-    const [key, setKey] = React.useState(Date.now());
-
-    return <TableListOpsHelper
-        key={key}
-        onChanged={changed => {
-            setChanged(changed as [])
-        }}
-        initData={changed}
+  return (
+    <TableListOpsHelper
+      key={key}
+      onChanged={changed => {
+        setChanged(changed as []);
+      }}
+      initData={changed}
     >
-        <div className="table-search-container" style={{
-            margin: '18px 0px',
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            height: '100%'
-        }}>
-            <div>
-                <Row>
-                    <Col span={12}>
-                        <div style={{
-                            display: "flex",
-                        }}>
-                            <InputWithoutIcon style={{ width: "290px" }} name="keyword"
-                                placeholder="输入关键词进行搜索"></InputWithoutIcon>
-                            <ColorButton width="76px" bgColor="#4084F0"
-                                onClick={() => props.onSearch(changed)}>查询</ColorButton>
-                            <ColorButton width="76px" bgColor="#FFFFFF" fontColor="#72757B" onClick={() => {
-                                setChanged([])
-                                setKey(Date.now())
-                                props.onSearch([])
-                            }}>清空</ColorButton>
-                        </div>
-                    </Col>
-                </Row>
-            </div>
-            <div>
-                <Row>
-                    <Col xl={2} xs={4} style={{ color: '#9099A2' }}>立案时间</Col>
-                    <Col>
-                        <OptionsDateRangePicker name={["createDateStart", "createDateEnd"]}></OptionsDateRangePicker>
-                    </Col>
-                </Row>
-            </div>
-            <div>
-                <Row>
-                    <Col xl={2} xs={4} style={{ color: '#9099A2' }}>结案时间</Col>
-                    <Col>
-                        <OptionsDateRangePicker name={["endDateStart", "endDateEnd"]}></OptionsDateRangePicker>
-                    </Col>
-                </Row>
-            </div>
+      <div
+        className="table-search-container"
+        style={{
+          margin: "18px 0px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          height: "100%"
+        }}
+      >
+        <div>
+          <Row>
+            <Col span={12}>
+              <div
+                style={{
+                  display: "flex"
+                }}
+              >
+                <InputWithoutIcon
+                  style={{ width: "290px" }}
+                  name="keyword"
+                  placeholder="输入关键词进行搜索"
+                ></InputWithoutIcon>
+                <ColorButton
+                  width="76px"
+                  bgColor="#4084F0"
+                  onClick={() => props.onSearch(changed)}
+                >
+                  查询
+                </ColorButton>
+                <ColorButton
+                  width="76px"
+                  bgColor="#FFFFFF"
+                  fontColor="#72757B"
+                  onClick={() => {
+                    setChanged([]);
+                    setKey(Date.now());
+                    props.onSearch([]);
+                  }}
+                >
+                  清空
+                </ColorButton>
+              </div>
+            </Col>
+          </Row>
         </div>
+        <div>
+          <Row>
+            <Col xl={2} xs={4} style={{ color: "#9099A2" }}>
+              立案时间
+            </Col>
+            <Col>
+              <OptionsDateRangePicker
+                name={["createDateStart", "createDateEnd"]}
+              ></OptionsDateRangePicker>
+            </Col>
+          </Row>
+        </div>
+        <div>
+          <Row>
+            <Col xl={2} xs={4} style={{ color: "#9099A2" }}>
+              结案时间
+            </Col>
+            <Col>
+              <OptionsDateRangePicker
+                name={["endDateStart", "endDateEnd"]}
+              ></OptionsDateRangePicker>
+            </Col>
+          </Row>
+        </div>
+      </div>
     </TableListOpsHelper>
-}
+  );
+};
